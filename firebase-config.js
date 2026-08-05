a5191c0db493e04590f4a7de3039591b163b8bd5
ToolVerseUSA <91483031+ToolVerseUSA@users.noreply.github.com>
2026-08-05 20:08:58 +0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // ToolVerse Firebase Configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAKYuPIPpwkS70HQ9jc3OdsNiTIDnEnAM8",
+    apiKey: "AIzaSyAKYuPIPpwkSZOHQ9jc3OdaNitIDnEnAM8",
     authDomain: "toolverstoken.firebaseapp.com",
     projectId: "toolverstoken",
     storageBucket: "toolverstoken.firebasestorage.app",
     messagingSenderId: "762644487097",
     appId: "1:762644487097:web:dbd8d858e1dc6f3c1bb468",
-    measurementId: "G-6VX2WPNEC2"
+    measurementId: "G-6VX2WPNECZ"
 };
 
 // Initialize Firebase
